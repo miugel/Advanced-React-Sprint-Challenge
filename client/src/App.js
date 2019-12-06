519a4ffbdbf076a49b4cd9c04a84cf257ce7dfdf
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-06 11:44:33 -0500

    answered questions, organized files, and layed out components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Table from './Table';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			players: []
+		}
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:5000/api/players')
+			.then(response => {
+				console.log(response);
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	}
+
+	render() {
+		return (
+			<div className='app'>
+				<h1>Top Google Searches</h1>
+				<h2>2019 Women's World Cup</h2>
+				<Table/>
+			</div>
+		)
+	}
 }
 
 export default App;
